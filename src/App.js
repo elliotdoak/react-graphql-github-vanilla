28db0c4850a202741151d4e1c518966d83f8bf02
ElliotDoak <elliot.doak@atos.net>
2022-05-04 12:01:48 +0100

    Create react app and build HTTP query for github api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const axiosGitHubGraphQL = axios.create({
+  baseURL: "https://api.github.com/graphql",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  },
+  withCredentials: false,
+  credentials: "same-origin",
+});
+
+const GET_ORGANISATION = `{
+  organization(login: "the-road-to-learn-react"){
+    name
+    url
+  }
+}`;
+
+const TITLE = "React GraphQL GitHub Client";
+
+export const App = () => {
+  const [url, setUrl] = useState("no url");
+  const [org, setOrg] = useState("no org");
+  const [err, setErr] = useState("no error");
+
+  useEffect(() => {
+    axiosGitHubGraphQL.post("", { query: GET_ORGANISATION }).then((res) => {
+      setOrg(res.data.data.organization);
+      setErr(res.data.errors);
+      console.log(res.data);
+    });
+  }, []);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log("org: ", org);
+    console.log("err: ", err);
+  };
+
+  const onChange = (event) => {
+    setUrl(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{TITLE}</h1>
+
+      <form onSubmit={onSubmit}>
+        <label htmlFor="url">Show open issues for https://github.com/</label>
+        <input id="url" onChange={onChange}></input>
+        <button type="submit">Search</button>
+      </form>
+
+      {url && <h2>{url}</h2>}
     </div>
   );
-}
+};
 
 export default App;
