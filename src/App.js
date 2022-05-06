73fb995c1a7e748a469719d964e9432a43edc364
ElliotDoak <elliot.doak@atos.net>
2022-05-06 09:58:15 +0100

    Missed changes from prev commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Organisation } from "./components/organisation";
+import "./styles.css";
 
 const axiosGitHubGraphQL = axios.create({
   baseURL: "https://api.github.com/graphql",
@@ -7,14 +9,27 @@ const axiosGitHubGraphQL = axios.create({
     "Content-Type": "application/json",
     Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
   },
-  withCredentials: false,
+  //withCredentials: false,
   credentials: "same-origin",
 });
 
-const GET_ORGANISATION = `{
+const GET_ISSUES_OF_REPO = `{
   organization(login: "the-road-to-learn-react"){
-    name
-    url
+   name
+   url
+   repository(name: "the-road-to-learn-react"){
+     name
+     url
+     issues(last:5){
+       edges {
+         node {
+           id
+           title
+           url
+         }
+       }
+     }
+   }
   }
 }`;
 
@@ -22,21 +37,20 @@ const TITLE = "React GraphQL GitHub Client";
 
 export const App = () => {
   const [url, setUrl] = useState("no url");
-  const [org, setOrg] = useState("no org");
-  const [err, setErr] = useState("no error");
+  const [org, setOrg] = useState({});
+  const [errors, setErrors] = useState([]);
 
   useEffect(() => {
-    axiosGitHubGraphQL.post("", { query: GET_ORGANISATION }).then((res) => {
-      setOrg(res.data.data.organization);
-      setErr(res.data.errors);
-      console.log(res.data);
+    axiosGitHubGraphQL.post("", { query: GET_ISSUES_OF_REPO }).then((res) => {
+      setOrg(res?.data?.data?.organization);
+      setErrors(res.data.errors ? res.data.errors : "");
     });
   }, []);
 
   const onSubmit = (event) => {
     event.preventDefault();
     console.log("org: ", org);
-    console.log("err: ", err);
+    console.log("err: ", errors);
   };
 
   const onChange = (event) => {
@@ -45,15 +59,26 @@ export const App = () => {
 
   return (
     <div className="App">
-      <h1>{TITLE}</h1>
+      <h1 className="Page">{TITLE}</h1>
 
-      <form onSubmit={onSubmit}>
+      <form className="form" onSubmit={onSubmit}>
         <label htmlFor="url">Show open issues for https://github.com/</label>
         <input id="url" onChange={onChange}></input>
         <button type="submit">Search</button>
       </form>
 
-      {url && <h2>{url}</h2>}
+      {org?.name && <Organisation org={org} errors={errors} />}
+
+      {errors && (
+        <div className="errorContainer">
+          <strong>Something went wrong:</strong>
+          <ul>
+            {errors.map((error) => (
+              <li key={error.message}>{error.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
